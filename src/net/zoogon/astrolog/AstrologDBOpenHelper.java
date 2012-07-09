9f2c3650745dd363903ca585f5282370177ee47d
Gonzalo Cao Cabeza de Vaca <gonzalo.cao@gmail.com>
2012-07-09 20:37:04 +0200

    adding all new files interactively

---
+++ src/net/zoogon/astrolog/AstrologDBOpenHelper.java
@@ -0,0 +1,77 @@
+package net.zoogon.astrolog;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class AstrologDBOpenHelper extends SQLiteOpenHelper {
+
+	// DB schema metadata
+	private static final String DATABASE_NAME = "AstrologDB.db";
+	private static final int DATABASE_VERSION = 1;
+
+	// DB table names
+	private static final String DATABASE_SESSIONS_TABLE = "sessions";
+
+	// DB columns names
+	private static final String SESSION_ID = "_id";
+	private static final String SESSION_TITLE = "title";
+	private static final String SESSION_DATE = "date";
+	private static final String SESSION_LOCATION = "location";
+	private static final String SESSION_NOTES = "notes";
+
+	// DB schema creation
+	private static final String DATABASE_CREATE = "create table "
+			+ DATABASE_SESSIONS_TABLE + " ("
+			+ SESSION_ID + " integer primary key autoincrement, "
+			+ SESSION_TITLE	+ " text not null, "
+			+ SESSION_DATE + " date not null, "
+			+ SESSION_LOCATION + " text not null, "
+			+ SESSION_NOTES	+ " text not null "
+			+ " );";
+
+	public AstrologDBOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	// Called when no database exists in disk and the helper class needs
+	// to create a new one.
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(DATABASE_CREATE);
+	}
+
+	// Called when there is a database version mismatch meaning that
+	// the version of the database on disk needs to be upgraded to
+	// the current version.
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Log the version upgrade.
+		Log.w("TaskDBAdapter", "Upgrading from version " + oldVersion + " to "
+				+ newVersion + ", which will destroy all old data");
+		// Upgrade the existing database to conform to the new
+		// version. Multiple previous versions can be handled by
+		// comparing oldVersion and newVersion values.
+		// The simplest case is to drop the old table and create a new one.
+		db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_SESSIONS_TABLE);
+
+		// TODO
+		// KEEP ATENTION OLD DATA WILL BE DROPPED BY THIS METHOD.
+		// NEWER VERSIONS SHOULD MIGRATE OLD DATA TO NEW DB SCHEMA
+	}
+
+	/**
+	 * cleanUp function empties database tables keeping schema. Use carefully
+	 * first declared as private to "avoid" temptations. Change to public if
+	 * needed.
+	 */
+
+	private void cleanUP(SQLiteDatabase db) {
+		//TODO
+		//db.execSQL("DELETE FROM " + DATABASE_SESSIONS_TABLE);
+
+	}
+}
